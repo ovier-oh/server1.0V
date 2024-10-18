a002ccdcca4d52c4761486cace157ba0031e8212
Ovier <ovier.obregon.hin@outlook.com>
2024-10-19 00:13:56 +0300

    creando login para administrar usuarios

--- server.py
+++ server.py
@@ -1,12 +1,18 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_KEY'] = 'my_secret_key'
 
 db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 migrate = Migrate(app, db)
 
 
@@ -14,30 +20,66 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
 @app.route('/')
+@login_required
 def index():
-    users = User.query.all()
-    return render_template('index.html', users=users)
+    if current_user.is_authenticated:
+        users = User.query.all()
+        return render_template('index.html', users=users)
+    return redirect(url_for('login'))
 
 
 @app.route('/add', methods=['POST'])
+@login_required
 def add_user():
-    name = request.form['name']
-    email = request.form['email']
-    new_user = User(name=name, email = email)
-    db.session.add(new_user)
-    db.session.commit()
+    if current_user.is_authenticated:
+        name = request.form['name']
+        email = request.form['email']
+        password = generate_password_hash(request.form['password'], method='sha256')
+        new_user = User(name=name, email=email, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        flash('User added successfully')
     return redirect(url_for('index'))
 
 
 @app.route('/delete/<int:id>')
+@login_required
 def delete_user(id):
     user = User.query.get_or_404(id)
     db.session.delete(user)
     db.session.commit()
+    flash('user deleted successfully!')
     return redirect(url_for('index'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        user = User.query.filter_by(email=email).first()
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            return redirect(url_for('index'))
+        else:
+            flash('Login failed. Check your email or password.')
+        return render_template('login.html')
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
 
 
 if __name__ == '__main__':
