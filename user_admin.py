729c84d9d6cb7b0d3e3f7bec9a43ce5d6b5d4c23
Ovier <ovier.obregon.hin@outlook.com>
2024-10-19 11:02:32 +0300

    agregando estilo a todas las paginas

--- user_admin.py
+++ user_admin.py
@@ -4,8 +4,8 @@ from werkzeug.security import generate_password_hash
 # Asegurarse de que las operaciones de base de datos estén dentro del contexto de la aplicación
 with app.app_context():
     # Crear el primer usuario administrador
-    admin = User(name="Admin", email="admin@example.com",
-                 password=generate_password_hash("adminpassword", method='pbkdf2:sha256'))
+    admin = User(name="OvierObregon", email="ovier@example.com",
+                 password=generate_password_hash("root1234", method='pbkdf2:sha256'))
 
     # Agregar el usuario administrador a la base de datos
     db.session.add(admin)
